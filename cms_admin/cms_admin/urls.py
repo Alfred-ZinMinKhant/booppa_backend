55da93168aefb96ac401fd96ef6d1ffe8532453c
Alfred-ZinMinKhant <zinminkhant.alfred@gmail.com>
2026-01-24 19:34:09 +0700

    Update url

--- cms_admin/cms_admin/urls.py
+++ cms_admin/cms_admin/urls.py
@@ -13,5 +13,4 @@ urlpatterns = [
     path("api/public/blogs/<slug:slug>/", cms_views.public_blog_detail),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
