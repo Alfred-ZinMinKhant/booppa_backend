209c1c10c58802ca2ecab566cd007e4c87c5e31b
Alfred-ZinMinKhant <zinminkhant.alfred@gmail.com>
2026-01-27 21:28:15 +0700

    update cms

--- cms_admin/cms/admin.py
+++ cms_admin/cms/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django import forms
+from django.urls import reverse
+from django.utils.html import format_html
+
 from .models import BlogPost, BlogImage, DemoBooking, SupportTicket, SupportTicketReply
 from django.utils.html import format_html
 
@@ -61,10 +65,33 @@ class DemoBookingAdmin(admin.ModelAdmin):
 
 @admin.register(SupportTicket)
 class SupportTicketAdmin(admin.ModelAdmin):
-    list_display = ("ticket_id", "email", "subject", "status", "priority", "created_at")
+    list_display = ("ticket_id", "email", "subject", "status", "priority", "created_at", "reply_link")
     search_fields = ("ticket_id", "email", "subject")
     list_filter = ("status", "priority", "category")
-    readonly_fields = ("ticket_id", "tracking_token", "created_at", "updated_at")
+    readonly_fields = ("ticket_id", "tracking_token", "created_at", "updated_at", "reply_link")
+    fields = (
+        "ticket_id",
+        "tracking_token",
+        "name",
+        "email",
+        "category",
+        "subject",
+        "message",
+        "status",
+        "priority",
+        "assigned_to",
+        "ip_address",
+        "user_agent",
+        "created_at",
+        "updated_at",
+        "reply_link",
+    )
+
+    def reply_link(self, obj):
+        url = reverse("admin:cms_supportticketreply_add")
+        return format_html('<a href="{}?ticket_id={}">Reply to this ticket</a>', url, obj.ticket_id)
+
+    reply_link.short_description = "Quick reply"
 
 
 @admin.register(SupportTicketReply)
@@ -72,3 +99,28 @@ class SupportTicketReplyAdmin(admin.ModelAdmin):
     list_display = ("ticket_id", "author", "author_type", "is_internal", "created_at")
     search_fields = ("ticket_id", "author")
     list_filter = ("author_type", "is_internal")
+    readonly_fields = ("id", "created_at")
+    fields = ("ticket_id", "author", "author_type", "message", "is_internal")
+
+    class ReplyForm(forms.ModelForm):
+        ticket_id = forms.ChoiceField(choices=())
+
+        class Meta:
+            model = SupportTicketReply
+            fields = ("ticket_id", "author", "author_type", "message", "is_internal")
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            tickets = SupportTicket.objects.order_by("-created_at").values_list(
+                "ticket_id", "ticket_id"
+            )
+            self.fields["ticket_id"].choices = list(tickets)
+
+    form = ReplyForm
+
+    def get_changeform_initial_data(self, request):
+        initial = {"author": "BOOPPA Support", "author_type": "staff"}
+        ticket_id = request.GET.get("ticket_id")
+        if ticket_id:
+            initial["ticket_id"] = ticket_id
+        return initial
